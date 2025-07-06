14c38caf33843fc28dac887abb2efe02b604d8bd
Hiral Goyal 17 <goyalbalkrishna9@gmail.com>
2025-07-06 12:33:47 +0000

    it's again same

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,10 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import 'dotenv/config';
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      apiKey: process.env.GOOGLE_API_KEY,
+      apiKey: process.env.NEXT_PUBLIC_GOOGLE_AI_KEY,
     }),
   ],
   logLevel: 'debug',
